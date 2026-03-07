04641c2a411382ca5264008462450f084fbccafe
Nathanael Prasetyo <nathanaelprasetyo.np@gmail.com>
2026-03-07 12:57:19 +0800

    RAW admin dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useAuthStore } from './stores/authStore'
-
 import Header from './components/Header.vue'
 import BottomNav from './components/BottomNav.vue'
-import LoginView from './views/LoginView.vue'      // import login
-
-const authStore = useAuthStore()
-const isAuth = computed(() => authStore.isAuthenticated)
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
-    <!-- header tetap tampil kalau sudah login -->
-    <Header v-if="isAuth" />
+    <Header />
 
     <main class="content-area bg-[#FFFFFF] grow">
-      <!-- kalau belum login, tampilkan login view,
-           kalau sudah, pakai router-view untuk sisa halaman -->
-      <LoginView v-if="!isAuth" />
-      <router-view v-else />
+      <router-view />
     </main>
 
-    <BottomNav v-if="isAuth" />
+    <BottomNav />
   </div>
 </template>
